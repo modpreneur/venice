e6c4faf929efd68b296be54a5c1954352ff6bafd
Jakub Fajkus <jakub.fajkus@gmail.com>
2015-12-21 19:23:50 +0100

    default controller cleanup

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,21 +15,15 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     *
-     *
      * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
-        $necktieGateway = $this->get("app.services.necktie_gateway");
-        $user = $this->getUser();
-
-        $necktieGateway->updateProductAccesses($user);
-        $necktieGateway->getInvoices($user);
-
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        return $this->render(
+            'default/index.html.twig',
+            [
+                'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            ]
+        );
     }
-
 }
