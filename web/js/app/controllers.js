78a6ac55f07761f54fcbf9ec78b5c7fb58875f52
Martin Kolek <prodejledu@gmail.com>
2015-12-30 18:28:51 +0100

    js build

--- web/js/app/controllers.js
+++ web/js/app/controllers.js
@@ -1,7 +1,7 @@
-import ProductController from "app/Controllers/ProductController.js";
 import BlogArticleController from "app/Controllers/BlogArticleController.js";
 import ContentController from "app/Controllers/ContentController.js";
 import ContentProductController from "app/Controllers/ContentProductController.js";
-
-var controllers = {};
+import ProductController from "app/Controllers/ProductController.js";
+import UserController from "app/Controllers/UserController.js";
+var controllers = {"BlogArticleController":BlogArticleController,"ContentController":ContentController,"ContentProductController":ContentProductController,"ProductController":ProductController,"UserController":UserController};
 export default controllers;
