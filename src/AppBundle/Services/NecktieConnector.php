8f307346ec1be775a0f34ba50e642cdebc5bf0e9
Jakub Fajkus <jakub.fajkus@gmail.com>
2016-01-26 11:32:21 +0100

    NecktieConnector edits based on tests

--- src/AppBundle/Services/NecktieConnector.php
+++ src/AppBundle/Services/NecktieConnector.php
@@ -15,6 +15,7 @@ use GuzzleHttp\Client;
 use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\ServerException;
+use Psr\Http\Message\MessageInterface;
 
 class NecktieConnector
 {
@@ -22,9 +23,36 @@ class NecktieConnector
     protected $client;
 
 
+    /**
+     * @return ClientInterface
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+
+    /**
+     * Set client object
+     *
+     * @param Client $client
+     */
+    public function setClient(Client $client)
+    {
+        $this->client = $client;
+    }
+
+
+    /**
+     * Create a client with $baseUri and set ti to the $client property
+     *
+     * @param string $baseUri
+     */
     public function setBaseUri(string $baseUri)
     {
-        $this->client = new Client(["base_uri" => $baseUri]);
+        $client = new Client(["base_uri" => $baseUri]);
+
+        $this->setClient($client);
     }
 
 
@@ -32,10 +60,9 @@ class NecktieConnector
      * @param User $user
      * @param $method string
      * @param $uri string
-     * @param array $parameters
+     * @param array $data
      * @param null|string $accessToken
      * @param bool $sendAsJson
-     * @param bool $jsonDecodeResponse
      *
      * @return null|string
      * @throws UnsuccessfulNecktieResponseException
@@ -45,22 +72,18 @@ class NecktieConnector
         User $user = null,
         string $method,
         string $uri,
-        array $parameters = [],
+        array $data = [],
         string $accessToken = null,
-        bool $sendAsJson = false,
-        bool $jsonDecodeResponse = true
+        bool $sendAsJson = false
     )
     {
         $accessToken = $this->getAccessToken($user, $accessToken);
-        $options = $this->prepareOptions($method, $parameters, $accessToken, $sendAsJson);
+        $options = $this->prepareOptions($method, $data, $accessToken, $sendAsJson);
 
         $response = null;
+
         try {
-            $response = $this->client->request(
-                $method,
-                $uri,
-                $options
-            );
+            $response = $this->createRequest($method, $uri, $options);
         } catch (ServerException $e) {
             throw new UnsuccessfulNecktieResponseException($e->getResponse()->getBody()->getContents());
         }
@@ -68,57 +91,90 @@ class NecktieConnector
             if($e->getCode() === 404) {
                 return null;
             }
+
+            throw new UnsuccessfulNecktieResponseException($e->getResponse()->getBody()->getContents());
         }
 
-        if($jsonDecodeResponse) {
-            return json_decode($response->getBody()->getContents(), true);
-        }
+        return $this->getBodyFromResponse($response);
+    }
 
-        return $response->getBody()->getContents();
+
+    /**
+     * Create request and return response
+     *
+     * @param string $method
+     * @param string $uri
+     * @param array $options
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    protected function createRequest(string $method, string $uri, array $options = [])
+    {
+        return $this->client->request(
+            $method,
+            $uri,
+            $options
+        );
     }
 
     /**
      * Get User's access token or provided access token
      *
-     * @param $user
-     * @param $accessToken
+     * @param User $user
+     * @param string $accessTokenString
+
      * @return mixed
      * @throws \Exception
      */
-    protected function getAccessToken(User $user = null, string $accessToken = null)
+    protected function getAccessToken(User $user = null, string $accessTokenString = null)
     {
-        if ($user) {
-            $accessToken = $user->getLastAccessToken();
-        } else if (!$accessToken) {
+        if ($user !== null) {
+            $usersAccessToken = $user->getLastAccessToken();
+
+            if($usersAccessToken !== null) {
+                return $usersAccessToken;
+            }
+        }
+
+
+        if ($accessTokenString === null) {
             throw new \Exception("User has no access token or the given accessToken is null.");
         }
 
-        return $accessToken;
+        return $accessTokenString;
     }
 
     /**
      * Prepare options array.
      *
      * @param $method
-     * @param $parameters
+     * @param $data
      * @param $accessToken
      * @param $sendAsJson
      * @return array
      */
-    protected function prepareOptions($method, $parameters, $accessToken, $sendAsJson)
+    protected function prepareOptions(string $method, array $data = [], string $accessToken = null, bool $sendAsJson = false)
     {
         $options = ["headers" => ["Authorization" => "Bearer {$accessToken}"],];
 
         if (strtolower($method) === "get") {
-            $options["query"] = $parameters;
+            $options["query"] = $data;
         } else if (strtolower($method) === "post") {
             if ($sendAsJson) {
-                $options["json"] = $parameters;
+                $options["json"] = $data;
             } else {
-                $options["form_params"] = $parameters;
+                $options["form_params"] = $data;
             }
         }
 
         return $options;
+    }
+
+    /**
+     * @param MessageInterface $response
+     * @return string
+     */
+    protected function getBodyFromResponse(MessageInterface $response)
+    {
+        return $response->getBody()->getContents();
     }
 }
