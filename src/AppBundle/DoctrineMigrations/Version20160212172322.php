f2792d7d0324ab58b350319abf24de325ff56554
Jakub Fajkus <jakub.fajkus@gmail.com>
2016-02-12 17:48:35 +0100

    remove useless statements from last migration

--- src/AppBundle/DoctrineMigrations/Version20160212172322.php
+++ src/AppBundle/DoctrineMigrations/Version20160212172322.php
@@ -21,9 +21,6 @@ class Version20160212172322 extends AbstractMigration
         $this->addSql('CREATE TABLE blog_article_product (blog_article_id INT NOT NULL, product_id INT NOT NULL, INDEX IDX_8C613C999452A475 (blog_article_id), INDEX IDX_8C613C994584665A (product_id), PRIMARY KEY(blog_article_id, product_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE blog_article_product ADD CONSTRAINT FK_8C613C999452A475 FOREIGN KEY (blog_article_id) REFERENCES blog_article (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE blog_article_product ADD CONSTRAINT FK_8C613C994584665A FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE product_access CHANGE from_date from_date DATETIME NOT NULL, CHANGE to_date to_date DATETIME DEFAULT NULL');
-        $this->addSql('DROP INDEX unique_name ON trinity_settings');
-        $this->addSql('CREATE UNIQUE INDEX unique_name ON trinity_settings (name)');
     }
 
     /**
@@ -35,8 +32,5 @@ class Version20160212172322 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE blog_article_product');
-        $this->addSql('ALTER TABLE product_access CHANGE from_date from_date DATETIME NOT NULL, CHANGE to_date to_date DATETIME DEFAULT NULL');
-        $this->addSql('DROP INDEX unique_name ON trinity_settings');
-        $this->addSql('CREATE UNIQUE INDEX unique_name ON trinity_settings (name)');
     }
 }
