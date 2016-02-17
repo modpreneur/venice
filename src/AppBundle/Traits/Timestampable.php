c878d0a57edd2f9588e164a46e29b82c2078bf10
Jakub Fajkus <jakub.fajkus@gmail.com>
2016-02-17 13:03:00 +0100

    create timestampable trait

---
+++ src/AppBundle/Traits/Timestampable.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jakub Fajkus
+ * Date: 16.02.16
+ * Time: 14:25
+ */
+
+namespace AppBundle\Traits;
+
+
+use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Model\Timestampable\TimestampableMethods;
+
+
+trait Timestampable
+{
+    use TimestampableMethods;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime", nullable=false)
+     */
+    protected $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime", nullable=false)
+     */
+    protected $updatedAt;
+
+}
