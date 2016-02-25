707dbd26962b4d3d9972ab07a2888975588a393b
Jakub Fajkus <jakub.fajkus@gmail.com>
2016-02-25 16:22:10 +0100

    Create class VeniceKernel. AppKernel will extend the VeniceKernel which has defined dependencies on another bundles.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,78 +1,7 @@
 <?php
 
-use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\HttpKernel\Kernel;
 
-class AppKernel extends Kernel
+class AppKernel extends \AppBundle\Kernel\VeniceKernel
 {
-    public function registerBundles()
-    {
-        $bundles = array(
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new KMS\FroalaEditorBundle\KMSFroalaEditorBundle(),
-            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
-            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
-            new FOS\RestBundle\FOSRestBundle(),
-            new JMS\SerializerBundle\JMSSerializerBundle(),
-            new \Snc\RedisBundle\SncRedisBundle(),
 
-            new \Knp\Bundle\MenuBundle\KnpMenuBundle(),
-
-            new AppBundle\AppBundle(),
-            new AdminBundle\AdminBundle(),
-            new FrontBundle\FrontBundle(),
-
-            new WhiteOctober\BreadcrumbsBundle\WhiteOctoberBreadcrumbsBundle(),
-            new Knp\Bundle\GaufretteBundle\KnpGaufretteBundle(),
-
-            // Trinity
-            new Trinity\FrameworkBundle\TrinityFrameworkBundle(),
-            new Trinity\NotificationBundle\TrinityNotificationBundle(),
-            new Trinity\AdminBundle\TrinityAdminBundle(),
-            new \Trinity\Bundle\SettingsBundle\SettingsBundle(),
-            new Trinity\Bundle\SearchBundle\SearchBundle(),
-            new Trinity\Bundle\GridBundle\GridBundle(),
-        );
-
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-        }
-
-        return $bundles;
-    }
-
-
-    public function getRootDir()
-    {
-        return __DIR__;
-    }
-
-
-    public function getCacheDir()
-    {
-        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
-    }
-
-
-    public function getLogDir()
-    {
-        return dirname(__DIR__).'/var/logs';
-    }
-
-
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
-    }
 }
