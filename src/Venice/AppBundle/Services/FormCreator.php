214ec24024984a0c19d5455c28cbfaa8007c2c14
Fajkus <jakub.fajkus@gmail.com>
2017-02-10 15:05:12 +0100

    Fix error with FormCreator

--- src/Venice/AppBundle/Services/FormCreator.php
+++ src/Venice/AppBundle/Services/FormCreator.php
@@ -2,8 +2,6 @@
 
 namespace Venice\AppBundle\Services;
 
-use Symfony\Component\Form\FormFactoryInterface;
-use Symfony\Component\Routing\RouterInterface;
 use Trinity\FrameworkBundle\Services\TrinityFormCreator;
 
 /**
@@ -11,17 +9,4 @@ use Trinity\FrameworkBundle\Services\TrinityFormCreator;
  */
 class FormCreator extends TrinityFormCreator
 {
-    /**
-     * FormFactory constructor.
-     *
-     * @param FormFactoryInterface $formFactory
-     * @param RouterInterface      $router
-     */
-    public function __construct(FormFactoryInterface $formFactory, RouterInterface $router)
-    {
-        parent::__construct($formFactory, $router);
-
-        $this->formFactory = $formFactory;
-        $this->router = $router;
-    }
 }
